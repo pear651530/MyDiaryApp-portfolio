9983dffd1c68b41499efc5e028037cc8296d53c9
pear651530 <candy651530@gmail.com>
2023-05-10 00:58:24 +0800

    20230510

--- Kimochi Nikki/app/src/main/java/com/example/kimochinikki/MainActivity.kt
+++ Kimochi Nikki/app/src/main/java/com/example/kimochinikki/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.kimochinikki
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var imageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageView = findViewById(R.id.load)
+        Glide.with(this).load(R.drawable.load).into(imageView)
+
     }
 }
