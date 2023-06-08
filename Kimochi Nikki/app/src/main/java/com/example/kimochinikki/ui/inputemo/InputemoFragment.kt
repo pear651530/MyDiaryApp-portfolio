8e1d1530f13f30e938dfff52f794acede95b9990
pear651530 <candy651530@gmail.com>
2023-06-09 02:28:34 +0800

    20230609_0228

--- Kimochi Nikki/app/src/main/java/com/example/kimochinikki/ui/inputemo/InputemoFragment.kt
+++ Kimochi Nikki/app/src/main/java/com/example/kimochinikki/ui/inputemo/InputemoFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.kimochinikki.databinding.FragmentInputemoBinding
 import com.example.kimochinikki.R
+import java.lang.Integer.max
 
 class InputemoFragment : Fragment() {
 
@@ -18,57 +20,6 @@ class InputemoFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    val QuotationsSimleId = intArrayOf(
-        R.string.quotations_simle1,
-        R.string.quotations_simle2,
-        R.string.quotations_simle3,
-        R.string.quotations_simle4,
-        R.string.quotations_simle5,
-        R.string.quotations_simle6,
-        R.string.quotations_simle7,
-        R.string.quotations_simle8,
-        R.string.quotations_simle9,
-        R.string.quotations_simle10
-    )
-
-    val QuotationsSadId = intArrayOf(
-        R.string.quotations_sad1,
-        R.string.quotations_sad2,
-        R.string.quotations_sad3,
-        R.string.quotations_sad4,
-        R.string.quotations_sad5,
-        R.string.quotations_sad6,
-        R.string.quotations_sad7,
-        R.string.quotations_sad8,
-        R.string.quotations_sad9,
-        R.string.quotations_sad10
-    )
-
-    val SuggestionsSimleId = intArrayOf(
-        R.string.suggestions_simle1,
-        R.string.suggestions_simle2,
-        R.string.suggestions_simle3,
-        R.string.suggestions_simle4,
-        R.string.suggestions_simle5,
-        R.string.suggestions_simle6,
-        R.string.suggestions_simle7,
-        R.string.suggestions_simle8,
-        R.string.suggestions_simle9,
-        R.string.suggestions_simle10
-    )
-
-    val SuggestionsSadId = intArrayOf(
-        R.string.suggestions_sad1,
-        R.string.suggestions_sad2,
-        R.string.suggestions_sad3,
-        R.string.suggestions_sad4,
-        R.string.suggestions_sad5,
-        R.string.suggestions_sad6,
-        R.string.suggestions_sad7,
-        R.string.suggestions_sad8,
-        R.string.suggestions_sad9,
-        R.string.suggestions_sad10
-    )
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -77,7 +28,58 @@ class InputemoFragment : Fragment() {
 
         _binding = FragmentInputemoBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        init()
+        val imageButton_Simle = binding.ImageButtonSimle
+        imageButton_Simle.setOnClickListener { addtime("Simle") }
+        val imageButton_Sad = binding.ImageButtonSad
+        imageButton_Sad.setOnClickListener { addtime("Sad") }
+        val imageButton_Angry = binding.ImageButtonAngry
+        imageButton_Angry.setOnClickListener { addtime("Angry") }
+        val imageButton_Heart = binding.ImageButtonHeart
+        imageButton_Heart.setOnClickListener { addtime("Heart") }
+
+        culavg()
         return root
+    }
+
+    fun init() {
+        binding.TextViewSimletime.text = "0"
+        binding.TextViewSadtime.text = "0"
+        binding.TextViewAngrytime.text = "0"
+        binding.TextViewHearttime.text = "0"
+    }
+
+    fun addtime(whice: String) {
+        if (whice == "Simle") {
+            var num = Integer.parseInt(binding.TextViewSimletime.text.toString())
+            num++
+            binding.TextViewSimletime.text = num.toString()
+        } else if (whice == "Sad") {
+            var num = Integer.parseInt(binding.TextViewSadtime.text.toString())
+            num++
+            binding.TextViewSadtime.text = num.toString()
+        } else if (whice == "Angry") {
+            var num = Integer.parseInt(binding.TextViewAngrytime.text.toString())
+            num++
+            binding.TextViewAngrytime.text = num.toString()
+        } else {
+            var num = Integer.parseInt(binding.TextViewHearttime.text.toString())
+            num++
+            binding.TextViewHearttime.text = num.toString()
+        }
+    }
+
+    public fun culavg() {
+        max(
+            max(
+                Integer.parseInt(binding.TextViewSimletime.text.toString()),
+                Integer.parseInt(binding.TextViewSadtime.text.toString())
+            ),
+            max(
+                Integer.parseInt(binding.TextViewAngrytime.text.toString()),
+                Integer.parseInt(binding.TextViewHearttime.text.toString())
+            )
+        )
     }
 
     override fun onDestroyView() {
